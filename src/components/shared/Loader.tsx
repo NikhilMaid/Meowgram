2bd0f6d9a1d529fda15f3372441bd6ce3c8af1bd
Nikhil Maid <nikhilmaid318@gmail.com>
2024-07-03 13:31:13 +0530

    adding files

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,16 @@
+
+
+const Loader = () => {
+  return (
+    <div>
+        <img 
+            src="/assets/icons/loader.svg" 
+            alt="loader" 
+            height={24} 
+            width={24} 
+        />
+    </div>
+  )
+}
+
+export default Loader
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,13 @@
+// import React from "react";
+import { z } from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Too Short" }),
+  username: z.string().min(2, { message: "Too Short" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." }),
+});
+
+// export default SignupValidation;
